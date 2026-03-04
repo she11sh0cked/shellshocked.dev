797d12a3867a94c757ca93eac897ff5b3e28d5ea
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2026-03-04 11:19:57 +0100

    chore(scripts): sync secrets loader shell compatibility logic

--- scripts/secrets_loader.sh
+++ scripts/secrets_loader.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-for var in $(env | grep _FILE= | cut -d= -f1); do
-    secret_name=$(echo $var | sed -r "s/_FILE//")
+for var in $(env | grep '_FILE=' | cut -d= -f1); do
+    secret_name=$(echo "$var" | sed 's/_FILE//')
     secret_file=$(eval echo \$$var)
-    secret_value=$(cat $secret_file)
+    secret_value=$(cat "$secret_file")
 
-    if [[ $secret_file =~ ^/run/secrets/ ]]; then
-        export "$secret_name"="$secret_value"
-    fi
+    case $secret_file in
+        /run/secrets/*)
+            export "$secret_name"="$secret_value"
+            ;;
+    esac
 done
 
 exec "$@"
